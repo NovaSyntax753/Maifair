06c7d80006e98924123b17b0550b865535308bdb
NovaSyntax753 <tejasdhok3@gmail.com>
2026-04-26 14:05:00 +0530

    Revert About page video to horizontal layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -183,7 +183,7 @@ export default function AboutPage() {
           </AnimatedSection>
 
           <AnimatedSection delay={0.2}>
-            <div className="relative aspect-[9/16] max-w-sm mx-auto border border-[rgba(201,168,76,0.2)] overflow-hidden">
+            <div className="relative aspect-video border border-[rgba(201,168,76,0.2)] overflow-hidden">
               <video
                 src="https://res.cloudinary.com/dvda87mbm/video/upload/q_auto,w_1280/v1777027398/lv_0_20260417143311_vurz1h.mp4"
                 controls
